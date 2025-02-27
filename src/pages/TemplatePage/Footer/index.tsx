a433ccd1c9a9e4fd2e97f93bd467ebd32c2d0239
eatthecookies <Aleksander.Vorobjev1@yandex.ru>
2025-02-27 23:53:45 +0300

    Added base styles to header & footer

--- src/pages/TemplatePage/Footer/index.tsx
+++ src/pages/TemplatePage/Footer/index.tsx
@@ -1,3 +1,8 @@
+import styles from "./Footer.module.css";
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div className={styles.footerContainer}>
+      <div className={styles.footer}>made by @eatthecookies</div>
+    </div>
+  );
 }
--- src/pages/TemplatePage/Header/index.tsx
+++ src/pages/TemplatePage/Header/index.tsx
@@ -1,3 +1,8 @@
+import styles from "./Header.module.css";
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <div className={styles.headerContainer}>
+      <div className={styles.header}>Hrader</div>
+    </div>
+  );
 }
