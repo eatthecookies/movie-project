15c1e242d359909b3d2bc6e37ee3309fe5e68c80
eatthecookies <Aleksander.Vorobjev1@yandex.ru>
2025-03-02 23:54:35 +0300

    Updated header

--- src/pages/TemplatePage/Header/index.tsx
+++ src/pages/TemplatePage/Header/index.tsx
@@ -1,8 +1,29 @@
+import { Link } from "react-router";
 import styles from "./Header.module.css";
+type Menu = {
+  [key: string]: string;
+};
 export default function Header() {
+  const menu: Menu = {
+    Фильмы: "/movies",
+    Сериалы: "/serials",
+    Актеры: "/persons",
+    КиноФортуна: "/fortune",
+    Тренды: "/popular",
+  };
   return (
     <div className={styles.headerContainer}>
-      <div className={styles.header}>Hrader</div>
+      <div className={styles.logo}>КиноХаос</div>
+      <div className={styles.links}>
+        {Object.keys(menu).map((item) => (
+          <Link to={menu[item]} key={item} className={styles.link}>
+            {item}
+          </Link>
+        ))}
+      </div>
+      <input></input>
+      <button>Найти</button>
+      <button> Темная тема</button>
     </div>
   );
 }
