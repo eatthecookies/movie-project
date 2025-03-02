f04f619d15175785f647ac61fb20b9df298027b2
eatthecookies <Aleksander.Vorobjev1@yandex.ru>
2025-03-02 23:54:24 +0300

    Small fixes

--- src/pages/TemplatePage/index.tsx
+++ src/pages/TemplatePage/index.tsx
@@ -1,10 +1,11 @@
 import { Outlet } from "react-router";
 import Header from "./Header";
 import Footer from "./Footer";
+import styles from "./Template.module.css";
 
 export default function TemplatePage() {
   return (
-    <div>
+    <div className={styles.page}>
       <Header />
       <Outlet></Outlet>
       <Footer />
