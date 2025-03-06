b3c38e785e077b48031d67f095d249cd5d645bec
eatthecookies <Aleksander.Vorobjev1@yandex.ru>
2025-03-06 23:10:53 +0300

    Added placeholder images in cards components

--- src/components/ActorCard/index.tsx
+++ src/components/ActorCard/index.tsx
@@ -2,16 +2,24 @@ import { Link } from "react-router";
 import { ActorResult } from "../../types/ActorTypes";
 import styles from "./ActorCard.module.css";
 
-export default function PosterCard({ actor }: { actor: ActorResult }) {
+export default function ActorCard({ actor }: { actor: ActorResult }) {
   const imageUrl = import.meta.env.VITE_API_IMAGE_URL;
 
   return (
-    <Link to={`person/${actor.id}`}>
+    <Link to={`/person/${actor.id}`}>
       <div className={styles.actorCard} key={actor.id}>
-        <img
-          className={styles.actorImg}
-          src={imageUrl + "/w500" + actor.profile_path}
-        ></img>
+        {actor.profile_path ? (
+          <img
+            className={styles.actorImg}
+            src={imageUrl + "/w500" + actor.profile_path}
+          ></img>
+        ) : (
+          <img
+            className={styles.actorImg}
+            src="https://icon-library.com/images/no-profile-pic-icon/no-profile-pic-icon-7.jpg"
+            alt=""
+          />
+        )}
         <p className={styles.actorName}>{actor.name}</p>
       </div>
     </Link>
--- src/components/PosterCard/index.tsx
+++ src/components/PosterCard/index.tsx
@@ -5,12 +5,22 @@ export default function PosterCard({ movie }: { movie: MovieResult }) {
   const imageUrl = import.meta.env.VITE_API_IMAGE_URL;
 
   return (
-    <Link to={`movie/${movie.id}`}>
-      <div key={movie.id}>
-        <img
-          className={styles.posterCard}
-          src={imageUrl + "/w500" + movie.poster_path}
-        ></img>
+    <Link to={`/movie/${movie.id}`}>
+      <div className={styles.card} key={movie.id}>
+        {movie.poster_path ? (
+          <img
+            className={styles.posterCard}
+            src={imageUrl + "/w500" + movie.poster_path}
+          ></img>
+        ) : (
+          <img
+            src="https://www.popcorn.app/assets/app/images/placeholder-movieimage.png"
+            className={styles.posterCard}
+          />
+        )}
+        <div className={styles.overlay}>
+          <p className={styles.movieTitle}>{movie.title}</p>
+        </div>
       </div>
     </Link>
   );
