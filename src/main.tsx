a6570c8792979c49f937f75337c83f09aca8ace6
eatthecookies <Aleksander.Vorobjev1@yandex.ru>
2025-03-06 16:55:58 +0300

    Installed axios and react-query

--- src/main.tsx
+++ src/main.tsx
@@ -6,16 +6,22 @@ import SearchPage from "./pages/SearchPage";
 import MoviePage from "./pages/MoviePage";
 import HomePage from "./pages/HomePage";
 import TemplatePage from "./pages/TemplatePage";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<TemplatePage />}>
-          <Route path="" element={<HomePage />} />
-          <Route path="/movie/:id" element={<MoviePage />} />
-          <Route path="/search" element={<SearchPage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<TemplatePage />}>
+            <Route path="" element={<HomePage />} />
+            <Route path="/movie/:id" element={<MoviePage />} />
+            <Route path="/search" element={<SearchPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   </StrictMode>
 );
